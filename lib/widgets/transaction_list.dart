053fb710b486a0b0ba927760f5ac1a088b4946ac
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2021-07-20 07:36:25 +0200

    Aggiunta ListView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,54 +4,54 @@ import 'package:flutter_personal_expense/models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...transactions.map((tx) => Card(
-          child: Row(
-            children: [
-              Container(
-                child: Text(
-                  '€ ${tx.amount.toString()}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple),
-                ),
-                margin: EdgeInsets.symmetric(
-                    horizontal: 10, vertical: 15),
-                decoration: BoxDecoration(
-                    border:
-                    Border.all(width: 2, color: Colors.purple)),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Text(
-                    //DateFormat.yMMMMd("it_IT").format(tx.date),
-                    DateFormat.yMMMMd().format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey,
+                  Container(
+                    child: Text(
+                      '€ ${transactions[index].amount.toString()}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
                     ),
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 2, color: Colors.purple)),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transactions[index].title,
+                        style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(
+                        //DateFormat.yMMMMd("it_IT").format(tx.date),
+                        DateFormat.yMMMMd().format(transactions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
-          ),
-        ))
-      ],
+              ),
+            );
+          }),
     );
   }
 }
