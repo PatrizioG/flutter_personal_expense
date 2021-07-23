9c756aebb2e4fc0ad0e27cd1d65e23ffaa7d4bfb
Patrizio Gasperi <patrizio.gasperi@gmail.com>
2021-07-23 07:29:03 +0200

    invertiti giorni sostituita ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,56 +14,41 @@ class TransactionList extends StatelessWidget {
       height: 300,
       child: transactions.isEmpty
           ? Column(
-        children: [
-          Text('No transactions added yet!', 
-          style: Theme.of(context).textTheme.headline6,),
-          SizedBox(height:10),
-          Container(
-            height: 200,
-            child: Image.asset('assets/images/waiting.png'),
-          ),
-        ],        
-      )
+              children: [
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 200,
+                  child: Image.asset('assets/images/waiting.png'),
+                ),
+              ],
+            )
           : ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          '€ ${transactions[index].amount.toString()}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 2,
-                                color: Theme.of(context).primaryColor)),
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(transactions[index].title,
-                              style: Theme.of(context).textTheme.headline6
-                              //TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                              ),
-                          Text(
-                            //DateFormat.yMMMMd("it_IT").format(tx.date),
-                            DateFormat.yMMMMd()
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                            child: Text(
+                                '€ ${transactions[index].amount.toString()}'),
+                          ),
+                        )),
+                    title: Text(transactions[index].title,
+                        style: Theme.of(context).textTheme.headline6
+                      //TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
               }),
